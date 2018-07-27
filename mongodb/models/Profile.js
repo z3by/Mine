16f10d4318610c2891aebc4823255097cd1a999f
zoaby <zoaby.am@gmail.com>
2018-07-27 09:26:05 +0300

    create the Profile model

---
+++ mongodb/models/Profile.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// define the user model
+const ProfileSchema = Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	firstname: {
+		required: true,
+		type: String,
+	},
+	lastname: {
+		required: true,
+		type: String,
+	},
+	handle: {
+		required: true,
+		type: String,
+		max: 40,
+	},
+	country: String,
+	city: String,
+	status: String,
+	bio: String,
+	job: String,
+	links: {
+		youtube: {
+			type: String
+		},
+		facebook: {
+			type: String
+		},
+		twitter: {
+			type: String
+		},
+		instagram: {
+			type: String
+		},
+		linkedin: {
+			type: String
+		},
+		googleplus: {
+			type: String
+		}
+	},
+	phone1: String,
+	phone2: String,
+	date: {
+		type: Date,
+		default: Date.now
+	}
+
+});
+
+const Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile;
