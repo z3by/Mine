fc69c3791e1afb0347065230e0d5e71feace44c8
zoaby <zoaby.am@gmail.com>
2018-07-27 09:24:34 +0300

    create the Url model

---
+++ mongodb/models/Url.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// define the user model
+const UrlSchema = Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	text: String,
+	avatar: String,
+	name: String,
+	likes: [String],
+	comments: [{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'users'
+		},
+		text: String,
+		category: String,
+		date: {
+			type: Date,
+			default: Date.now
+		}
+	}],
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+const Url = mongoose.model('Url', UrlSchema);
+module.exports = Url;
